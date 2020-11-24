d7fbd62b9e04522d3152376ab6f6768b69a16d54
これで勝つる！ <shuntacurosu@gmail.com>
2020-11-24 08:28:28 +0000

    finished up to Step 16 of Section 5.

--- src/main/java/com/in28minutes/springboot/WelcomeService.java
+++ src/main/java/com/in28minutes/springboot/WelcomeService.java
@@ -1,11 +1,16 @@
 package com.in28minutes.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WelcomeService {
+    
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+        
 	public String retrieveWelcomeMessage() {
-		// Complex Method
-		return "Good Morning";
+        // Complex Method
+		return welcomeMessage;
 	}
 }
--- src/main/java/com/in28minutes/springboot/controller/SurveyController.java
+++ src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -1,28 +1,57 @@
 package com.in28minutes.springboot.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.in28minutes.springboot.model.Question;
 import com.in28minutes.springboot.service.SurveyService;
 
 @RestController
 public class SurveyController {
-	
-	@Autowired
-	private SurveyService surveyService;
-	
-	@GetMapping("/surveys/{surveyId}/questions")
-	public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId) {
-		return surveyService.retrieveQuestions(surveyId);
-	}
-	
-	@GetMapping("/surveys/{surveyId}/questions/{questionId}")
-	public Question retrieveDetailsForQuestion(@PathVariable String surveyId, @PathVariable String questionId) {
-		return surveyService.retrieveQuestion(surveyId, questionId);
-	}
+
+    @Autowired
+    private SurveyService surveyService;
+
+    @GetMapping("/surveys/{surveyId}/questions")
+    public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId) {
+        return surveyService.retrieveQuestions(surveyId);
+    }
+
+    @GetMapping("/surveys/{surveyId}/questions/{questionId}")
+    public Question retrieveDetailsForQuestion(@PathVariable String surveyId, @PathVariable String questionId) {
+        return surveyService.retrieveQuestion(surveyId, questionId);
+    }
+
+    @PostMapping("/surveys/{surveyId}/questions")
+    public ResponseEntity<Void> addQuestionToSurvey(@PathVariable String surveyId,
+            @RequestBody Question newQuestion) {
+
+        // What should be structure of request body?
+        // How will it be mapped to Question object?RequestBody
+        // What should be returned?
+        // What should be response status?
+        Question question = surveyService.addQuestion(surveyId, newQuestion);
+
+        if (question == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        // Success - URI of the new resource in Response Header
+        // Status created
+        // URI -> /surveys/{surveyId}/questions/{questionId} question.getQuestionId()
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(question.getId())
+                .toUri();
+
+        // Status
+        return ResponseEntity.created(location).build();
+    }
 }
